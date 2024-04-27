cbc3e488fc8fa15ee96e444cc65c024423029740
vitatriutami <vitatriutami@gmail.com>
2024-04-27 11:57:54 +0700

    adding link

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+// NPM PACKAGE - Typed.js 
 const Typed = require("typed.js");
 
 const element = new Typed(".typing-text", {
@@ -5,11 +6,21 @@ const element = new Typed(".typing-text", {
     "Vita",
     "a Longlife Learner",
     "a Devscale student",
-    "a Software Engineer wannabe!"],
+    "a Software Engineer"],
   typeSpeed: 100,
   loop: true,
   showcursor: true,
   cursorChar: "|",
   startDelay: 1000,
   fadeOut: true
+});
+
+// FILTERED NAV
+const navLinks = document.querySelector('li');
+
+navLinks.forEach(navLink => {
+  navLink.addEventListener('click', () => {
+    document.querySelector('.active:text-blue-500')?.classList.remove('.active:text-blue-500');
+    navLink.classList.add('.active:text-blue-500');
+  });
 });
