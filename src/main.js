7223f40fbdf03de596f2a6d4ef836f4632a9aec0
vitatriutami <vitatriutami@gmail.com>
2024-04-28 17:10:44 +0700

    adding update

--- src/main.js
+++ src/main.js
@@ -5,8 +5,8 @@ const element = new Typed(".typing-text", {
   strings: [
     "Vita",
     "a Longlife Learner",
-    "a Devscale student",
-    "a Software Engineer"],
+    "based in Indonesia",
+  ],
   typeSpeed: 100,
   loop: true,
   showcursor: true,
@@ -16,11 +16,27 @@ const element = new Typed(".typing-text", {
 });
 
 // FILTERED NAV
-const navLinks = document.querySelector('li');
+// const tgtLink = document.querySelector('.active:text-blue-500');
 
-navLinks.forEach(navLink => {
-  navLink.addEventListener('click', () => {
-    document.querySelector('.active:text-blue-500')?.classList.remove('.active:text-blue-500');
-    navLink.classList.add('.active:text-blue-500');
+// navLinks.forEach(navLink => {
+//   navLink.addEventListener('click', () => {
+//     tgtLink?.classList.remove('active:text-blue-500');
+//     navLink.classList.add('active:text-blue-500');
+//   });
+// });
+
+
+// FILTERED NAV
+const filterNavs = document.querySelectorAll('li');
+
+filterNavs.forEach(filterNav => {
+  filterNav.addEventListener('click', () => {
+    filterNav.classList.add('opacity-100');
   });
 });
+
+
+// filterNav.addEventListener('click', e => {
+//   filterNav?.classList.remove('opacity-100');
+//   e.target.classList.add('opacity-100');
+// });
