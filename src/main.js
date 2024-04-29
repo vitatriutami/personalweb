424b27b4e741ac81471c34a0d24475a84bfaff54
vitatriutami <vitatriutami@gmail.com>
2024-04-29 09:10:24 +0700

    updating folder

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-// NPM PACKAGE - Typed.js 
 const Typed = require("typed.js");
+const confetti = require("canvas-confetti");
+
+
+// NPM PACKAGE - Typed.js 
 
 const element = new Typed(".typing-text", {
   strings: [
-    "Fullstack Developer",
+    "Vita",
+    "a Fullstack Developer",
     "a Longlife Learner",
     "based in Indonesia",
   ],
@@ -13,4 +17,25 @@ const element = new Typed(".typing-text", {
   cursorChar: "|",
   startDelay: 1000,
   fadeOut: true
+});
+
+// Confetti
+const helloBtn = document.getElementById('helloBtn');
+
+helloBtn.addEventListener("click", () => {
+  const myCanvas = document.createElement("canvas");
+  myCanvas.style.width = "100vw";
+  myCanvas.style.height = "100vh";
+  myCanvas.style.position = "fixed";
+  myCanvas.style.inset = 0;
+  myCanvas.style.zIndex = 9999;
+
+  document.body.appendChild(myCanvas);
+
+  const myConfetti = confetti.create(myCanvas, { resize: true });
+
+  myConfetti({
+    particleCount: 500,
+    spread: 160
+  });
 });
